2c089715d50683d70a8c8a31377cb7e2c97a1b72
BlockOG <alesiobaca@gmail.com>
2022-06-14 20:24:45 +0300

    Fix tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -4,14 +4,13 @@ import cell_machine_levels, unittest
 class TestLevel(unittest.TestCase):
     def test_open(self):
         test = cell_machine_levels.level.Level(10, 10, "test", 2)
-        test[0, 0, True] = True
-        test[0, 1, False] = cell_machine_levels.level.Cell(
+        test[0, 0] = True
+        test[0, 1] = cell_machine_levels.level.Cell(
             cell_machine_levels.level.CellEnum.mover,
             cell_machine_levels.level.Rotations.up,
         )
-        self.assertEqual(
-            test, cell_machine_levels.level.open("V1;10;10;0.9;3.3.0.8;test;2")
-        )
+        test1 = cell_machine_levels.level.open("V1;10;10;0.0;3.3.0.1;test;2")
+        self.assertEqual(test, test1)
 
 
 if __name__ == "__main__":
