0028837aca4094663c368027e3cad05832733b77
BlockOG <68442822+BlockOG@users.noreply.github.com>
2022-06-14 20:53:39 +0000

    Add save to tests, pylint really hates the tests

--- tests/__main__.py
+++ tests/__main__.py
@@ -9,8 +9,16 @@ class TestLevel(unittest.TestCase):
             cell_machine_levels.level.CellEnum.mover,
             cell_machine_levels.level.Rotations.up,
         )
-        test1 = cell_machine_levels.level.open("V1;10;10;0.0;3.3.0.1;test;2")
-        self.assertEqual(test, test1)
+        self.assertEqual(test, cell_machine_levels.level.open("V1;10;10;0.0;3.3.0.1;test;2"))
+    
+    def test_save(self):
+        test = cell_machine_levels.level.Level(10, 10, "test", 2)
+        test[0, 0] = True
+        test[0, 1] = cell_machine_levels.level.Cell(
+            cell_machine_levels.level.CellEnum.mover,
+            cell_machine_levels.level.Rotations.up,
+        )
+        self.assertEqual(test.save("V1"), "V1;10;10;0.0;3.3.0.1;name;2")
 
 
 if __name__ == "__main__":
