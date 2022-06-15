1e1350f16f20127fcf3e7c4b63aa4e9ebceb4c65
BlockOG <68442822+BlockOG@users.noreply.github.com>
2022-06-15 12:04:07 +0300

    HOW DID THE TESTS PASS

--- cell_machine_levels/V1.py
+++ cell_machine_levels/V1.py
@@ -45,4 +45,4 @@ def save(level: Level) -> str:
         if cell.type != 0:
             cells.append(f"{int(cell.type)}.{int(cell.rotation)}.{x}.{y}")
 
-    return f"V1;{level.width};{level.height};{';'.join(placeable)};{';'.join(cells)};{level.name};{int(level.wall_effect)}"
+    return f"V1;{level.width};{level.height};{','.join(placeable)};{','.join(cells)};{level.name};{int(level.wall_effect)}"
