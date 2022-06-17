7bfbb6bdc5280aca46e9e7631398a9179f2d9567
BlockOG <alesiobaca@gmail.com>
2022-06-17 17:23:45 +0300

    Now fixed

--- cell_machine_levels/__main__.py
+++ cell_machine_levels/__main__.py
@@ -7,7 +7,7 @@ get_level = lambda: cell_machine_levels.level.open(get_level_code())
 
 get_format = lambda: input("Format to output in (V1, V2 or V3): ")
 
-choices = [
+menus = [
     [
         [
             exit,
@@ -60,18 +60,23 @@ choices = [
     ],
 ]
 
-current_menu = 0
+def main():
+    """Main CLI."""
+    current_menu = 0
 
-while True:
-    # Choices
-    for i, j in enumerate(choices[current_menu][1]):
-        print(f"{i}. {j}")
-    print()
+    while True:
+        # Choices
+        for i, j in enumerate(menus[current_menu][1]):
+            print(f"{i}. {j}")
+        print()
 
-    try:
-        # Get and run choice
-        choice = int(input("Enter your choice: "))
-        current_menu = choices[current_menu][0][choice]()
-    except (ValueError, IndexError):
-        print("Invalid choice")
-        continue
+        try:
+            # Get and run choice
+            choice = int(input("Enter your choice: "))
+            current_menu = menus[current_menu][0][choice]()
+        except (ValueError, IndexError):
+            print("Invalid choice")
+            continue
+
+if __name__ == "__main__":
+    main()
